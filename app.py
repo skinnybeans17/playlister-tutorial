562a91e5856af21a664c8b8b3e61f17872e1fcf8
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-30 14:52:35 -0800

    Users can see all playlists

--- app.py
+++ app.py
@@ -8,5 +8,16 @@ def index():
     # change the original return statement you wrote to the one below
     return render_template('home.html', msg='Flask is Cool!!')
 
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
+
 if __name__ == '__main__':
     app.run(debug=True)
