e157aa716c77ead58b9dc94074ef9830a2a5742d
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-30 15:09:18 -0800

    s

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
@@ -8,13 +13,12 @@ def index():
     # change the original return statement you wrote to the one below
     return render_template('home.html', msg='Flask is Cool!!')
 
-# OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+#playlists = [
+#    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+#]
 
-@app.route('/playlists')
+@app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists)
